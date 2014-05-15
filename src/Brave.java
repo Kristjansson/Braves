f84f8f723132946dbbe4b559f432f843799aa736
Kristjansson <joseph.k.christianson@gmail.com>
2014-05-15 08:37:57 -0700

    Hi guise, I added a few println()s to the Brave class

--- src/Brave.java
+++ src/Brave.java
@@ -3,5 +3,9 @@ public class Brave {
 	public static void main(String[] args) {
 		LinePrinter lp = new LinePrinter("Hello, world!");
 		lp.multiPrint(10);
+		System.out.println("check");
+		System.out.println("Wow");
+		System.out.println("Much branch");
+		System.out.println("So GitHub");
 	}
 }
